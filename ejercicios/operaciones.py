0aad5cba9be2648af1a3c6b37464af97c9352e11
AngelfcProfe <157831892+AngelfcProfe@users.noreply.github.com>
2024-02-12 19:14:49 +0100

    Update operaciones.py

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -1,5 +1,11 @@
 # En principio el alumno solo recibiría la definición de la función en el archivo correspondiente
 def suma(x,y):
 
-    # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
+    # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente.
     return x + y
+
+def esPar(x):
+    if x%2 == 0:
+        return "par"
+    else:
+        return "impar"
